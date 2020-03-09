ed11dbeba54d126b4d6f0c7916ea40abd1ccabe1
qatang <qatang@gmail.com>
2020-03-09 14:30:15 +0800

    生成dnsmasq配置、设置ipset 脚本 #6

--- install.sh
+++ install.sh
@@ -1,46 +1,44 @@
 #!/bin/bash
 #
-# ubnt自愈能力安装脚本 FISH
+# ubnt自愈能力安装脚本
 #
 # 说明：
 #   * ubnt路由器在升级固件后会删除所有配置和数据，除了/config目录,导致每次升级后都需要大量人工操作。
 #   * 执行此脚本后，自动设置好所有配置。
 #   * 此脚本是安装程序，即安装后使ubnt具备自愈能力，故安装此脚本时需ubnt正常运转且已连接互联网。
 #   * 请切换到root用户执行此脚本。sudo su -
-echo "$(date +%Y-%m-%d\ %H:%M:%S) - Starting install.sh..."
+
+
+# 2. 第一次启动执行
+    
+
+# 3. 配置前执行
+
+
+# 4. 配置后执行
+#     更新dnsmasq配置
+
+
+
+PATH=/usr/local/sbin:/usr/sbin:/sbin:/usr/local/bin:/usr/bin:/bin
+export PATH
+
+function log() {
+    echo "$(date +%Y-%m-%d\ %H:%M:%S) - $1..."
+}
+
+log "starting install.sh"
 if [ ! -d /etc/ubnt ]; then
-    echo "This script is designed for running on UBNT/Unifi Routers"
-    exit
+    log "This script is designed for running on UBNT/Unifi Routers"
+    exit 1
 fi
-
-# 下载deb并创建link
-# $1 本地文件名
-# $2 下载地址
-function download_deb_link_firstboot() {
-    # downlaod deb
-    echo "$(date +%Y-%m-%d\ %H:%M:%S) - downloading $1..."
-    wget -O ${FISH_PACKAGES_DIR}/$1 $2
-
-    # link deb
-    echo "$(date +%Y-%m-%d\ %H:%M:%S) - linking ${FISH_PACKAGES_DIR}/$1 to ${UBNT_FIRSTBOOT_PKGS_DIR}/$1" 
-    ln -sf ${FISH_PACKAGES_DIR}/$1 ${UBNT_FIRSTBOOT_PKGS_DIR}/$1
-}
-
-# 下载数据
-# $1 本地文件名
-# $2 下载地址
-function download_data() {
-    echo "$(date +%Y-%m-%d\ %H:%M:%S) - downloading $1..."
-    wget -O ${FISH_DATA_DIR}/$1 $2
-}
 
 # 定义系统目录
 # UBNT开头的变量是EdgeOS默认自动执行或读取的目录
 # 根目录是/config，升级固件后数据不会丢失
-
+#
 # 在执行firstboot.d脚本前，自动安装目录中的*.deb
 UBNT_FIRSTBOOT_PKGS_DIR=/config/data/firstboot/install-packages
-
 # 三个启动时按顺序自动执行的脚本目录:
 #   /config/scripts/firstboot.d   升级完固件只执行一次
 #   /config/scripts/pre-config.d  每次重启后在加载config.boot之前执行
@@ -49,141 +47,286 @@ UBNT_FIRSTBOOT_DIR=/config/scripts/firstboot.d
 UBNT_PRECONFIG_DIR=/config/scripts/pre-config.d
 UBNT_POSTCONFIG_DIR=/config/scripts/post-config.d
 
+# 定义本地目录
+# LOCAL开头的变量是本地目录
+#
+# 根目录/config/user-data/local
+LOCAL_DIR=/config/user-data/local
+# 存放用户本地配置
+LOCAL_ETC_DIR=${LOCAL_DIR}/etc
+# 本地包目录
+LOCAL_PACKAGES_DIR=${LOCAL_DIR}/packages
+# 本地脚本目录
+LOCAL_SCRIPTS_DIR=${LOCAL_DIR}/scripts
+# 本地数据目录
+LOCAL_DATA_DIR=${LOCAL_DIR}/data
 
-# 定义安装目录
-# FISH开头的变量是安装目录
-# 根目录是/config/user-data/fish
+# 下载deb
+function add_deb() {
+    if [ $# -ne 3 ]; then
+        log "You must specify 3 args"
+        exit 1
+    fi
 
-# 存放自动生成的配置和用户本地配置
-# 与/etc对应，会生成link文件到/etc
-# 如果用户有自定义配置，自行放置到此目录
-# 比如，/config/user-data/fish/etc/dnsmasq.d/local_domain.conf
-FISH_ETC_DIR=/config/user-data/fish/etc
+    if [ ! -f ${LOCAL_PACKAGES_DIR}/$2 ]; then
+        log "downloading $2"
+        wget -O ${LOCAL_PACKAGES_DIR}/$2 $3
+    fi
 
-# 安装包目录
-# 比如，/user-data/fish/packages/wireguard-v2.0-e300-0.0.20191219-2.deb
-FISH_PACKAGES_DIR=/config/user-data/fish/packages
+    log "overwriting ${LOCAL_PACKAGES_DIR}/$2 to ${UBNT_FIRSTBOOT_PKGS_DIR}/$1"
+    cp -f ${LOCAL_PACKAGES_DIR}/$2 $UBNT_FIRSTBOOT_PKGS_DIR/$1
+}
 
-# 脚本目录
-# 安装程序生成的脚本都存放于此
-# 如果用户有自定义脚本，自行放置到此目录
-# 比如，/config/user-data/fish/scripts/local_crontab
-FISH_SCRIPTS_DIR=/config/user-data/fish/scripts
+# 创建文件夹
+function create_directories() {
+    log "creating UBNT directories"
+    mkdir -p $UBNT_FIRSTBOOT_PKGS_DIR $UBNT_FIRSTBOOT_DIR $UBNT_PRECONFIG_DIR $UBNT_POSTCONFIG_DIR
 
-# 数据目录
-# 比如，/config/user-data/fish/data/domain.list
-# /config/user-data/fish/data/ip.list
-FISH_DATA_DIR=/config/user-data/fish/data
+    log "creating LOCAL directories"
+    mkdir -p $LOCAL_ETC_DIR $LOCAL_PACKAGES_DIR $LOCAL_SCRIPTS_DIR $LOCAL_DATA_DIR
+}
 
-# 创建UBNT目录
-echo "$(date +%Y-%m-%d\ %H:%M:%S) - Creating the UBNT directories..."
-mkdir -p $UBNT_FIRSTBOOT_PKGS_DIR
-mkdir -p $UBNT_FIRSTBOOT_DIR
-mkdir -p $UBNT_PRECONFIG_DIR
-mkdir -p $UBNT_POSTCONFIG_DIR
+# 添加deb到初始化安装目录
+function add_debs_to_firstboot_install_packages() {
+    # wireguard
+    add_deb "wireguard.deb" "wireguard-v2.0-e300-0.0.20191219-2.deb" "https://github.com/Lochnair/vyatta-wireguard/releases/download/0.0.20191219-2/wireguard-v2.0-e300-0.0.20191219-2.deb"
+}
 
-# 创建FISH目录
-echo "$(date +%Y-%m-%d\ %H:%M:%S) - Creating the FISH directories..."
-mkdir -p $FISH_ETC_DIR
-mkdir -p $FISH_PACKAGES_DIR
-mkdir -p $FISH_SCRIPTS_DIR
-mkdir -p $FISH_DATA_DIR
+# 生成第一次启动执行的脚本
+function generate_firstboot_script() {
+    # 设置时区
+    script_name="00_first_boot_set_timezone.sh"
+    log "generating $script_name"
+    cat > $UBNT_FIRSTBOOT_DIR/$script_name <<- 'EOF'
+#!/bin/bash
+#
+# 设置时区
+#
 
-# 下载packages
-download_deb_link_firstboot "wireguard.deb" "https://github.com/Lochnair/vyatta-wireguard/releases/download/0.0.20191219-2/wireguard-v2.0-e300-0.0.20191219-2.deb"
+PATH=/usr/local/sbin:/usr/sbin:/sbin:/usr/local/bin:/usr/bin:/bin
+export PATH
 
-# 下载数据
-download_data "domain.list" "https://raw.githubusercontent.com/sskaje/ubnt-config/dev/proxy/domain.list"
-download_data "ip.list" "https://raw.githubusercontent.com/sskaje/ubnt-config/dev/proxy/ip.list"
-download_data "port.list" "https://raw.githubusercontent.com/sskaje/ubnt-config/dev/proxy/port.list"
+function log() {
+    echo "$(date +%Y-%m-%d\ %H:%M:%S) - $1..."
+}
 
-# 生成vyatta-local-env配置
-FISH_VYATTA_LOCAL_ENV_CONFIG=${FISH_ETC_DIR}/default/vyatta-local-env
-echo "$(date +%Y-%m-%d\ %H:%M:%S) - generating ${FISH_VYATTA_LOCAL_ENV_CONFIG}..."
-test -d ${FISH_ETC_DIR}/default || mkdir -p ${FISH_ETC_DIR}/default
-cat << EOF > $FISH_VYATTA_LOCAL_ENV_CONFIG
-export LANG=en_US.UTF-8
-export LC_ALL=C.UTF-8
-export TERM=rxvt
-alias ll="ls -alh"
+log "Starting $0" 
+
+# 设置系统时区
+log "Setting timezone Asia/Shanghai" 
+timedatectl set-timezone Asia/Shanghai
+
+log "finish ${0}" 
 EOF
+    log "finish generate $script_name"
 
-# 生成apt源配置
-FISH_STRETCH_LIST_CONFIG=${FISH_ETC_DIR}/apt/sources.list.d/stretch.list
-echo "$(date +%Y-%m-%d\ %H:%M:%S) - generating ${FISH_STRETCH_LIST_CONFIG}..."
-test -d ${FISH_ETC_DIR}/apt/sources.list.d || mkdir -p ${FISH_ETC_DIR}/apt/sources.list.d
-cat << EOF > $FISH_STRETCH_LIST_CONFIG
+    # 设置语言
+    script_name="01_first_boot_set_locale.sh"
+    log "generating $script_name"
+    cat > $UBNT_FIRSTBOOT_DIR/$script_name <<- 'EOF'
+#!/bin/bash
+#
+# 设置语言
+#
+
+PATH=/usr/local/sbin:/usr/sbin:/sbin:/usr/local/bin:/usr/bin:/bin
+export PATH
+
+function log() {
+    echo "$(date +%Y-%m-%d\ %H:%M:%S) - $1..."
+}
+
+log "Starting $0" 
+
+# 设置语言环境，支持中文
+log "replace LC_ALL=C to LC_ALL=C.UTF-8 in /etc/default/vyatta-local-env"
+locale -a | grep "C\.UTF\-8" > /dev/null && sed -i "/^export LC_ALL=C$/s/LC_ALL=C/LC_ALL=C.UTF-8/g" /etc/default/vyatta-local-env
+
+log "finish ${0}" 
+EOF
+    log "finish generate $script_name"
+
+    # 设置apt源
+    script_name="02_first_boot_set_sources.sh"
+    log "generating $script_name"
+    cat > $UBNT_FIRSTBOOT_DIR/$script_name <<- 'EOF'
+#!/bin/bash
+#
+# 设置apt源
+#
+
+PATH=/usr/local/sbin:/usr/sbin:/sbin:/usr/local/bin:/usr/bin:/bin
+export PATH
+
+function log() {
+    echo "$(date +%Y-%m-%d\ %H:%M:%S) - $1..."
+}
+
+log "Starting $0"
+
+# 更新apt源
+log "overwriting /etc/apt/sources.list.d/stretch.list"
+cat > /etc/apt/sources.list.d/stretch.list <<- 'SUBEOF'
 deb http://mirrors.huaweicloud.com/debian/ stretch main contrib 
 deb http://mirrors.huaweicloud.com/debian/ stretch-updates main contrib 
 deb http://mirrors.huaweicloud.com/debian/ stretch-backports main contrib  
 deb http://mirrors.huaweicloud.com/debian-security/ stretch/updates main 
+SUBEOF
+
+log "finish ${0}" 
 EOF
+    log "finish generate $script_name"
+}
 
-# 生成dnsmasq配置
-FISH_DNSMASQ_CONFIG=${FISH_ETC_DIR}/dnsmasq.d/domain.conf
-echo "$(date +%Y-%m-%d\ %H:%M:%S) - generating ${FISH_DNSMASQ_CONFIG}..."
-test -d ${FISH_ETC_DIR}/dnsmasq.d || mkdir -p ${FISH_ETC_DIR}/dnsmasq.d
-sed '/^#/d;/^$/d' ${FISH_DATA_DIR}/domain.list | awk -F , '{if($2!=""){print"server=/"$1"/"$2;}if($3!=""){print"ipset=/."$1"/"$3;}if($4!=""){print"address=/."$1"/"$4;}}' | tee ${FISH_DNSMASQ_CONFIG}
-
-# 生成init脚本
-test -d ${FISH_SCRIPTS_DIR}/firstboot.d || mkdir -p ${FISH_SCRIPTS_DIR}/firstboot.d
-FISH_INIT_SCRIPT=${FISH_SCRIPTS_DIR}/firstboot.d/fish_init.sh
-echo "$(date +%Y-%m-%d\ %H:%M:%S) - generating ${FISH_INIT_SCRIPT}..."
-cat << EOF > $FISH_INIT_SCRIPT
+# 生成加载配置前执行的脚本
+function generate_preconfig_script() {
+    # 设置别名
+    script_name="00_pre_config_set_alias.sh"
+    log "generating $script_name"
+    cat > $UBNT_PRECONFIG_DIR/$script_name <<- 'EOF'
 #!/bin/bash
 #
-# 初始化脚本
+# 设置别名
 #
 
-echo "$(date +%Y-%m-%d\ %H:%M:%S) - Starting /config/scripts/firstboot.d/fish_init.sh..." 
+PATH=/usr/local/sbin:/usr/sbin:/sbin:/usr/local/bin:/usr/bin:/bin
+export PATH
 
-# 设置系统时区
-echo "$(date +%Y-%m-%d\ %H:%M:%S) - Setting timezone Asia/Shanghai" 
-timedatectl set-timezone Asia/Shanghai
+function log() {
+    echo "$(date +%Y-%m-%d\ %H:%M:%S) - $1..."
+}
 
-# link vyatta_local_env
-echo "$(date +%Y-%m-%d\ %H:%M:%S) - linking $FISH_VYATTA_LOCAL_ENV_CONFIG to /etc/default/vyatta-local-env" 
-ln -sf $FISH_VYATTA_LOCAL_ENV_CONFIG /etc/default/vyatta-local-env
+log "Starting $0" 
 
-# link stretch.list
-echo "$(date +%Y-%m-%d\ %H:%M:%S) - linking $FISH_STRETCH_LIST_CONFIG to /etc/apt/sources.list.d/stretch.list" 
-ln -sf $FISH_STRETCH_LIST_CONFIG /etc/apt/sources.list.d/stretch.list
+# alias ll="ls -alh"
+log "append alias ll=\"ls -alh\" to /etc/default/vyatta-local-env"
+grep "alias ll=\"ls -alh\"" /etc/default/vyatta-local-env > /dev/null || sed -i '$a \alias ll="ls -alh"' /etc/default/vyatta-local-env
 
-echo "$(date +%Y-%m-%d\ %H:%M:%S) - finish /config/scripts/firstboot.d/fish_init.sh..." 
+log "finish ${0}" 
 EOF
+    log "finish generate $script_name"
 
-# 安装软件
-test -d ${FISH_SCRIPTS_DIR}/post-config.d || mkdir -p ${FISH_SCRIPTS_DIR}/post-config.d
-FISH_APT_INSTALL_SCRIPT=${FISH_SCRIPTS_DIR}/post-config.d/fish_apt_install.sh
-echo "$(date +%Y-%m-%d\ %H:%M:%S) - generating ${FISH_APT_INSTALL_SCRIPT}..."
-cat << EOF > $FISH_APT_INSTALL_SCRIPT
+    script_name="01_pre_config_link_etc.sh"
+    log "generating $script_name"
+    cat > $UBNT_PRECONFIG_DIR/$script_name <<- 'EOF'
 #!/bin/bash
 #
-# 安装软件脚本
+# local配置link到/etc
 #
 
-echo "$(date +%Y-%m-%d\ %H:%M:%S) - Starting apt install..." 
+PATH=/usr/local/sbin:/usr/sbin:/sbin:/usr/local/bin:/usr/bin:/bin
+export PATH
 
-echo "$(date +%Y-%m-%d\ %H:%M:%S) - updaing apt sources..." 
+function log() {
+    echo "$(date +%Y-%m-%d\ %H:%M:%S) - $1..."
+}
+
+log "Starting $0"
+
+LOCAL_DIR=/config/user-data/local
+LOCAL_ETC_DIR=${LOCAL_DIR}/etc
+
+log "linking $LOCAL_ETC_DIR to /etc" 
+function link_etc() {
+    cd $LOCAL_DIR
+    for i in `find etc -type f | grep -v "\.skip"`; do
+        log "link ${LOCAL_DIR}/$i to /$i"
+        ln -sf ${LOCAL_DIR}/$i /$i
+    done;    
+}
+link_etc
+
+log "finish ${0}" 
+EOF
+    log "finish generate $script_name"
+}
+
+# 生成加载配置后执行的脚本
+function generate_postconfig_script() {
+    # 安装软件
+    script_name="00_post_config_install_app.sh"
+    log "generating $script_name"
+    cat > $UBNT_POSTCONFIG_DIR/$script_name <<- 'EOF'
+#!/bin/bash
+#
+# 加载配置后脚本
+#
+
+PATH=/usr/local/sbin:/usr/sbin:/sbin:/usr/local/bin:/usr/bin:/bin
+export PATH
+
+function log() {
+    echo "$(date +%Y-%m-%d\ %H:%M:%S) - $1..."
+}
+
+log "Starting $0"
+
+log "updaing apt sources" 
 apt update -y
 
-# wget zip unzip dnsutils net-tools
-echo "$(date +%Y-%m-%d\ %H:%M:%S) - installing wget zip unzip dnsutils net-tools..." 
+echo "installing wget zip unzip dnsutils net-tools" 
 apt install -y wget zip unzip dnsutils net-tools
 
-echo "$(date +%Y-%m-%d\ %H:%M:%S) - finish apt install..." 
+log "finish ${0}" 
 EOF
+    log "finish generate $script_name"
 
-# 9. 生成dnsmasq配置、设置ipset 脚本
-#    下载*.list
+    script_name="00_post_config_install_app.sh"
+    log "generating $script_name"
+    cat > $UBNT_POSTCONFIG_DIR/$script_name <<- 'EOF'
+#!/bin/bash
+#
+# 加载配置后脚本
+#
 
-# 10. restart service脚本
+PATH=/usr/local/sbin:/usr/sbin:/sbin:/usr/local/bin:/usr/bin:/bin
+export PATH
 
-# 11. link
-#   packages link到UBNT_FIRSTBOOT_PKGS_DIR
-#   生成link etc
+function log() {
+    echo "$(date +%Y-%m-%d\ %H:%M:%S) - $1..."
+}
 
+log "Starting $0"
 
+LOCAL_DIR=/config/user-data/local
+LOCAL_DATA_DIR=${LOCAL_DIR}/data
 
+# 安装软件
+log "updaing apt sources" 
+apt update -y
 
-echo "$(date +%Y-%m-%d\ %H:%M:%S) - finish install.sh..." 
+echo "installing wget zip unzip dnsutils net-tools" 
+apt install -y wget zip unzip dnsutils net-tools
+
+# 生成dnsmasq配置
+wget -O ${LOCAL_DATA_DIR}/domain.list "https://raw.githubusercontent.com/sskaje/ubnt-config/dev/proxy/domain.list"
+
+FISH_DNSMASQ_CONFIG=${FISH_ETC_DIR}/dnsmasq.d/domain.conf
+UBNT_DNSMASQ_CONFIG=/etc/dnsmasq.d/domain.conf
+
+LOCAL_DNS=${$1:-`ip route show table 6 | awk '{print $3}'`}
+sed "/^#/d;/^$/d;s/8.8.8.8/${LOCAL_DNS}/g" ${FISH_DATA_DIR}/domain.list | awk -F , '{if($2!=""){print"server=/"$1"/"$2;}if($3!=""){print"ipset=/."$1"/"$3;}if($4!=""){print"address=/."$1"/"$4;}}' | tee ${FISH_DNSMASQ_CONFIG}
+
+echo "$(date +%Y-%m-%d\ %H:%M:%S) - linking $FISH_DNSMASQ_CONFIG to $UBNT_DNSMASQ_CONFIG" 
+ln -sf $FISH_DNSMASQ_CONFIG $UBNT_DNSMASQ_CONFIG
+
+echo "$(date +%Y-%m-%d\ %H:%M:%S) - restart dnsmasq.service" 
+# systemcrl restart dnsmasq.service
+# systemcrl status dnsmasq.service
+
+log "finish ${0}" 
+EOF
+    log "finish generate $script_name"
+}
+
+function install() {
+    create_directories
+    add_debs_to_firstboot_install_packages
+    generate_firstboot_script
+    generate_preconfig_script
+    generate_postconfig_script
+}
+
+install
+
+log "finish install.sh"
